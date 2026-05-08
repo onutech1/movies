4f5741d21169e12de094126e539ac307cddfee13
Praise john <praisejohn388@gmail.com>
2026-05-08 13:35:20 +0100

    done

--- src/Servies/ApiClient.js
+++ src/Servies/ApiClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "ea789d95b142473dbedee936017b4354",
+    key: import.meta.env.VITE_API_KEY,
   },
 });
